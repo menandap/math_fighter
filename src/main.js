868dd7f2025057bb5935bce3911c3275e4c2c279
Ananda Prema <anandaprema185@gmail.com>
2024-11-16 11:08:38 +0800

    up new 13

--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
 import Phaser from 'phaser'
 
 // import HelloWorldScene from './scenes/HelloWorldScene'
+import MathFighterScene from './scenes/MathFighterScene'
 
 const config = {
 	type: Phaser.AUTO,
-	width: 400,
-	height: 620,
-	physics: {
-		default: 'arcade'
-	},
+	width: 480,
+	height: 640,
 	scale: {
-		mode: Phaser.Scale.FIT,
+        mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 }
+		}
 	},
-	scene: [HelloWorldScen]
+	scene: [MathFighterScene]
 }
 
 export default new Phaser.Game(config)
